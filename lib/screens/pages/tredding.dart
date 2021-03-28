da2f61f067b09a88c8280aedf913fcf8d7929b48
Tiago_Frade <tiagofrade90@gmail.com>
2021-03-28 19:40:28 +0100

    Build list view card for Trending screen

--- lib/screens/pages/tredding.dart
+++ lib/screens/pages/tredding.dart
@@ -6,8 +6,60 @@ class Trending extends StatefulWidget {
 }
 
 class _TrendingState extends State<Trending> {
-      @override
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) => buildUserCard(),
+      ),
+    );
+  }
+
+  buildUserCard() {
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Card(
+        elevation: 5,
+        margin: EdgeInsets.all(1),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 20, bottom: 25, left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'name',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.70,
+                child: Text(
+                  'description',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                ),
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: <Widget>[
+                  Container(
+                      width: 30,
+                      height: 30,
+                      child: Text('imagem'),
+                  ),
+                  SizedBox(width: 5),
+                  Text('owner'),
+                  Spacer(),
+                  Icon(Icons.star_outlined, color: Colors.black),
+                  SizedBox(width: 5),
+                  Text('stars')
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
