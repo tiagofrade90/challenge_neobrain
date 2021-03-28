ddbb95fb5a86a434c369c12b5d975b13d7918fca
Tiago_Frade <tiagofrade90@gmail.com>
2021-03-28 19:10:57 +0100

    Remove back button on appbar of mainScreen

--- lib/screens/mianScreen.dart
+++ lib/screens/mianScreen.dart
@@ -19,6 +19,7 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true,
         title: Text('Main Screen'),
       ),
