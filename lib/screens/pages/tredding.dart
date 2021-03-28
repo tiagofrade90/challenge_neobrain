fa1a0e4c6ceee1e1f35173dad064e35069a6992c
Tiago_Frade <tiagofrade90@gmail.com>
2021-03-28 20:32:48 +0100

    Added processing of data received by the github apito be shown on the Tredding screen

--- lib/screens/pages/tredding.dart
+++ lib/screens/pages/tredding.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'package:challenge_neobrain/models/git_hub_reponse.dart';
 import 'package:flutter/material.dart';
+import '../../constants/constants.dart';
+import 'package:http/http.dart' as http;
+import 'package:challenge_neobrain/models/git_hub_reponse.dart';
+import 'package:challenge_neobrain/models/repository.dart';
 
 class Trending extends StatefulWidget {
   @override
@@ -7,16 +13,67 @@ class Trending extends StatefulWidget {
 
 class _TrendingState extends State<Trending> {
   @override
+  void initState() {
+    super.initState();
+  _getGitHubRepositories();
+  }
+
+  _getGitHubRepositories() async {
+    var queryParameters = {
+      'q': 'created:>2017-10-22',
+      'sort': 'stars',
+      'order': 'desc' 
+    };
+
+    var uri = Uri.https(BASE_GITHUB_URL, GITHUBSEARCH, queryParameters);
+    final reponse = await http.get(uri);
+
+    if (reponse.statusCode == 200) {
+      var reponseDecoded = GitHubReponse.fromJson(json.decode(reponse.body));
+      return reponseDecoded;
+    }
+    else {
+      print("Error");
+    }
+  }
+
+
+@override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _getGitHubRepositories(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return _buildBody(snapshot.data);
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return AlertDialog(
+            title: Text('Error'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Close"))
+            ],
+          );
+        }
+      },
+    );
+  }
+
+  Container _buildBody(GitHubReponse gitHubReponse) {
     return Container(
       child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) => buildUserCard(),
+        itemCount: gitHubReponse.items.length,
+        itemBuilder: (BuildContext context, int index) =>
+            buildUserCard(gitHubReponse.items[index]),
       ),
     );
   }
 
-  buildUserCard() {
+  buildUserCard(Repository repository) {
     return Padding(
       padding: const EdgeInsets.all(0),
       child: Card(
@@ -29,14 +86,14 @@ class _TrendingState extends State<Trending> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                'name',
+                repository.name ?? 'N/A',
                 style: TextStyle(fontSize: 20, fontFamily: 'Muli'),
               ),
               SizedBox(height: 5),
               Container(
                 width: MediaQuery.of(context).size.width * 0.70,
                 child: Text(
-                  'description',
+                  repository.description ?? 'N/A',
                   style: TextStyle(fontSize: 15, fontFamily: 'Muli'),
                 ),
               ),
@@ -46,16 +103,16 @@ class _TrendingState extends State<Trending> {
                   Container(
                       width: 30,
                       height: 30,
-                      child: Text('imagem'),
-                  ),
+                      child: Image.network(repository.owner.avatar_url)
+                      ),
                   SizedBox(width: 5),
-                  Text('owner', 
+                  Text(repository.owner.name ?? 'N/A', 
                     style: TextStyle(fontSize: 15, fontFamily: 'Muli'),
                   ),
                   Spacer(),
                   Icon(Icons.star_outlined, color: Colors.black),
                   SizedBox(width: 5),
-                  Text('stars')
+                  Text(repository.stars.toString() ?? 'N/A')
                 ],
               )
             ],
